a73493afcb544735d281d4ef9ffb0a9d5402cf47
jasperliew <yikpui@wtech.software>
2024-11-29 09:33:30 +0800

    commit-test-merge-4

--- App.vue
+++ App.vue
@@ -1,7 +1,7 @@
 <script>
 	export default {
 		onLaunch: function() { 
-			console.log('App Launch right now commit-test 2')
+			console.log('App Launch right now commit-test 3')
 		},
 		onShow: function() {
  
